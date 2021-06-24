e0f68413391a573e6bdc51cbe6271e24f8c0c771
ko-kamenashi <kamenashi0917@gmail.com>
2021-06-24 22:26:29 +0900

    local initial commit
    delete unnecessary files, update some setting and wrote some codes to try to be used it.

---
+++ office/src/excel/main.ts
@@ -0,0 +1,44 @@
+function main(workbook: ExcelScript.Workbook) {
+    // Get the current worksheet.
+    let selectedSheet = workbook.getActiveWorksheet();
+
+    /** Format the range to display numerical dollar amounts.
+     * 
+     * TODO:
+     * The format set by the following code is not applied,
+     *   but ¥ symbol and the other letters is applied instead of $.
+     */
+    selectedSheet.getRange("D2:E8").setNumberFormat("$#,##0.00");
+
+    // Fit the width of all the used columns to the data.
+    selectedSheet.getUsedRange().getFormat().autofitColumns();
+
+    // Get the value of cell D2.
+    // let range = selectedSheet.getRange("D2");
+    // console.log(range.getValues());
+
+    // Run the `Math.abs` function with the value at D2 and apply that value back to D2.
+    // let positiveValue = Math.abs(range.getValue() as number);
+    // range.setValue(positiveValue);
+
+    // Get the values of the used range.
+    let range       = selectedSheet.getUsedRange();
+    let rangeValues = range.getValues();
+
+    // Iterate over the fourth and fifth columns and set their values to their absolute value.
+    let rowCount = range.getRowCount();
+
+    for (let i = 1; i < rowCount; i++) {
+        // The column at index 3 is column "4" in the worksheet.
+        if (rangeValues[i][3] != 0) {
+            let positiveValue = Math.abs(rangeValues[i][3] as number);
+            selectedSheet.getCell(i, 3).setValue(positiveValue);
+        }
+
+        // The column at index 4 is column "5" in the worksheet.
+        if (rangeValues[i][4] != 0) {
+            let positiveValue = Math.abs(rangeValues[i][4] as number);
+            selectedSheet.getCell(i, 4).setValue(positiveValue);
+        }
+    }
+}

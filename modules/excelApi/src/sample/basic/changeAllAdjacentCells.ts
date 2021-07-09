4360aa77b4998dc31233d33863f23a51cc85e738
ko-kamenashi <kamenashi0917@gmail.com>
2021-07-09 21:17:55 +0900

    chore: add sample code
    Change all adjacent cells

---
+++ modules/excelApi/src/sample/basic/changeAllAdjacentCells.ts
@@ -0,0 +1,16 @@
+function main(workbook: ExcelScript.Workbook) {
+  // Get the active cell.
+  let activeCell = workbook.getActiveCell();
+
+  // Get the cell that's one row above and one column to the left of the active cell.
+  let cornerCell = activeCell.getOffsetRange(-1,-1);
+
+  // Get a range that includes all the cells surrounding the active cell.
+  let surroundingRange = cornerCell.getResizedRange(2, 2)
+
+  // Copy the formatting from the active cell to the new range.
+  surroundingRange.copyFrom(
+    activeCell, /* The source range. */
+    ExcelScript.RangeCopyType.formats /* What to copy. */
+  );
+}
